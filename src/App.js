2b58b0792a67d7b9c4a3f36d0ba7116d33d779bf
sschoe85 <s@bine-schoetensack.eu>
2020-08-20 19:12:20 +0200

    first Try at object recognition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+require('@tensorflow/tfjs-backend-cpu');
+require('@tensorflow/tfjs-backend-webgl');
+const cocoSsd = require('@tensorflow-models/coco-ssd');
+
 
 function App() {
+  function detectObjects(video, model) {
+    model.detect(video).then(predictions => {      
+      document.querySelector('.current-predictions').innerText = JSON.stringify(predictions, null, 2);
+    });
+    window.requestAnimationFrame(() => detectObjects(video, model));
+  }
+  
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true
+    })
+    .then(stream => {
+      const video = document.querySelector("#webcam");
+  
+      video.srcObject = stream;
+  
+      video.addEventListener("loadeddata", async() => {
+        const model = await cocoSsd.load();
+        let predictions = await detectObjects(video,model);
+      });
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Object Classification with TensorFlow.js</h1>
+          
+        <div class="responsive-embed">
+          <video id="webcam" autoplay></video>
+        </div>
+        
+        <pre class="current-predictions"></pre>
     </div>
   );
 }
