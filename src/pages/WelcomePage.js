264008687611cad33887060910aff371d8380208
sschoe85 <s@bine-schoetensack.eu>
2020-08-21 10:36:27 +0200

    Installed emotion (on wrong branch :)) and added pages folder

---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function WelcomePage() {
+    return (
+        <h1>Hello! This is your computer. Show me something and I will tell you what I see...</h1>
+        <button>Let's go!</button>
+    );
+}
+
+export default WelcomePage;
