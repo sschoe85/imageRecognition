dc49c4fe1104bd80526a7ec97e19dfc8693dc55e
sschoe85 <s@bine-schoetensack.eu>
2020-08-21 11:51:53 +0200

    Added Styling and Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { getMedia } from "./api/getMedia";
-import { useQuery } from "react-query";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+import VideoPage from "./pages/VideoPage";
 
 
 function App() {
  
 
   return (
-    
+    <div>
+      <Router>
+        <Switch>
+      <Route path = "/"><WelcomePage/></Route>
+      </Switch>
+    <Switch>
+      <Route path = "/video"><VideoPage/></Route>
+    </Switch>
+      </Router>
+    </div>
    
   );
 }
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React from "react";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 function WelcomePage() {
-    const StyledButton = styled.button`
-    border-radius: 50%;
-    background-color: black;
-    color: white;
-    font-size: 1.5em`
 
-    const StyledDiv = styled.div`
-    `
-    return (
-        <div>
-        <h1>Hello! This is your computer. Show me something and I will tell you what I see...</h1>
-        <button>Let's go!</button></div>
-    );
+
+
+  const StyledLink = styled(Link)`
+    
+    color: white;
+    font-weight: bold;
+    font-size: 2em;
+    text-decoration: none;
+    align-self: center;
+    justify-self: center;
+  `;
+
+const StyledDiv = styled.div`
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    background-color: blueviolet;
+    
+  `;
+  return (
+    <StyledDiv>
+      <h1>
+        Hello! This is your computer. Show me something and I will tell you what
+        I see...
+      </h1>
+      <StyledLink to ="/video">Let's go!</StyledLink>
+    </StyledDiv>
+  );
 }
 
 export default WelcomePage;
